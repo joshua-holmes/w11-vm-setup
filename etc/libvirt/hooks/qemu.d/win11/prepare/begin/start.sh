7abf2fed534df6cfbc850266e53f48e03fe588cf
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-07-13 22:58:26 -0700

    Cleanup and add useful code comments

--- etc/libvirt/hooks/qemu.d/win11/prepare/begin/start.sh
+++ etc/libvirt/hooks/qemu.d/win11/prepare/begin/start.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-/home/josh/check.sh "start before driver swap"
-# stop sddm
+# Stop window manager and display manager. Change to fit your setup
 killall Hyprland
 systemctl stop sddm.service
 
-# unbind from virtual consoles
+# Unbind from virtual consoles
 echo 0 > /sys/class/vtconsole/vtcon0/bind
 echo 0 > /sys/class/vtconsole/vtcon1/bind
 
-# unbind from efi-frambuffer
-# echo -n "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/unbind
-
-# echo -n "0000:09:00.0" > /sys/bus/pci/drivers/amdgpu/unbind
-# rmmod -f amdgpu
-# modprobe vfio_pci
-
+# Change id to match your GPU id using:
+# `lspci -k | grep -i -A 3 vga`
 gpu=0000:08:00.0
 aud=0000:08:00.1
 gpu_vd="$(cat /sys/bus/pci/devices/$gpu/vendor) $(cat /sys/bus/pci/devices/$gpu/device)"
 aud_vd="$(cat /sys/bus/pci/devices/$aud/vendor) $(cat /sys/bus/pci/devices/$aud/device)"
 
+# Unbind driver
 echo -n $gpu > /sys/bus/pci/devices/$gpu/driver/unbind
 echo -n $aud > /sys/bus/pci/devices/$aud/driver/unbind
 
-# echo -n $gpu_vd > /sys/bus/pci/drivers/amdgpu/remove_id
-# echo -n $aud_vd > /sys/bus/pci/drivers/amdgpu/remove_id
-
+# Bind new driver
 echo -n $gpu_vd > /sys/bus/pci/drivers/vfio-pci/new_id
 echo -n $aud_vd > /sys/bus/pci/drivers/vfio-pci/new_id
 
-/home/josh/check.sh "start after driver swap"
--- etc/libvirt/hooks/qemu.d/win11/release/end/stop.sh
+++ etc/libvirt/hooks/qemu.d/win11/release/end/stop.sh
@@ -1,41 +1,25 @@
 #!/bin/bash
 
-# echo 1 > /sys/bus/pci/devices/0000:01:00.0/remove
-# echo 1 > /sys/bus/pci/devices/0000:01:00.1/remove
-# echo 1 > /sys/bus/pci/rescan
-
-# reload amd driver
-# echo -n "0000:09:00.0" > /sys/bus/pci/drivers/vfio-pci/unbind
-# rmmod -f vfio_pci
-# modprobe -a amdgpu
-# echo -n "0000:09:00.0" > /sys/bus/pci/drivers/vfio-pci/unbind
-# echo -n "0000:09:00.0" > /sys/bus/pci/drivers/amdgpu/bind
-
-
-
-
-
-
+# Change id to match your GPU id using:
+# `lspci -k | grep -i -A 3 vga`
 gpu=0000:08:00.0
 aud=0000:08:00.1
 gpu_vd="$(cat /sys/bus/pci/devices/$gpu/vendor) $(cat /sys/bus/pci/devices/$gpu/device)"
 aud_vd="$(cat /sys/bus/pci/devices/$aud/vendor) $(cat /sys/bus/pci/devices/$aud/device)"
 
+# Unbind driver
 echo $gpu > /sys/bus/pci/devices/$gpu/driver/unbind
 echo $aud > /sys/bus/pci/devices/$aud/driver/unbind
 
-# echo $gpu_vd > /sys/bus/pci/drivers/vfio-pci/remove_id
-# echo $aud_vd > /sys/bus/pci/drivers/vfio-pci/remove_id
-
+# Bind new driver
 echo $gpu > /sys/bus/pci/drivers/amdgpu/bind
 echo $aud > /sys/bus/pci/drivers/snd_hda_intel/bind
 
-# attempt rebind to virtual consoles
+# Attempt rebind to virtual consoles
 echo 1 > /sys/class/vtconsole/vtcon0/bind
 echo 1 > /sys/class/vtconsole/vtcon1/bind
 
-# restart sddm
+# Restart window manager and display manager. Change to fit your setup
 systemctl start sddm.service
 hyprland-wrapped &
 
-/home/josh/check.sh "stop after driver swap"
